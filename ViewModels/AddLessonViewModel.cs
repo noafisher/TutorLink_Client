cb25c15cd4edda87d7efb8296e022c5a86111dd9
User <User@LAB2-12>
2025-03-10 10:32:10 +0200

    add lesson

--- ViewModels/AddLessonViewModel.cs
+++ ViewModels/AddLessonViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TutorLinkClient.Services;
+using TutorLinkClient.Models;
+using TutorLinkClient.Views;
 
 namespace TutorLinkClient.ViewModels
 {
     public class AddLessonViewModel : ViewModelBase
     {
+        private TutorLinkWebAPIProxy proxy;
+        private IServiceProvider serviceProvider;
+
+        private ;
 
     }
 }
